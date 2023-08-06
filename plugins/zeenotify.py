4473ac5aad6783cb89c3bc797f4842c0fe34c493
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-06 22:49:39 +0530

    Update zeenotify.py

--- plugins/zeenotify.py
+++ plugins/zeenotify.py
@@ -8,7 +8,6 @@ from config import ADMINS
 #for extract the new episode link
 import requests as ree
 from bs4 import BeautifulSoup
-import time
 
 '''
 Sample output(bunch link)
@@ -18,7 +17,8 @@ Sample output(bunch link)
 '''
 link1=[1]
 listlink=[1]
-def fun():
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('notify'))
+async def notify(client: Client, message: Message):
     while 1:
         #kannada serials link
         url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
@@ -38,10 +38,10 @@ def fun():
         newlist=list(new_link)
         for i in newlist:
             link1.append(i)
-            global b
             b=str(i)
-            print(f"https://www.zee5.com{b}\n\n")
-            time.sleep(2)
+            bot.send_message(message.chat.id, f"https://www.zee5.com{b}")
+            # print(f"https://www.zee5.com{b}\n\n")
+            .sleep(2)
             
         #here we delete the old episode link(premium free)
         duplicatelinks = set(link1).difference(set(listlink))
